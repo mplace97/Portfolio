eed028c4967397202d4eb52f095aa9ae8def53ad
Marco Placentino <139796348+mplace97@users.noreply.github.com>
2023-08-12 20:14:39 +0200

    add component Carrer

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import AboutSection from "@/components/AboutSection"
 import HeroSection from "@/components/HeroSection"
 import ProjectsSection from "@/components/ProjectsSection"
+import CareerTimeline from "@/components/CareerTimeline"
+
 export default function Home() {
   return (
     <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
       <HeroSection />
       <AboutSection />
+      <CareerTimeline/>
       <ProjectsSection />
     </main>
   )
